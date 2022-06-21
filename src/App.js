79945dd07bb4162a25cd422af232e9fd8543c9dd
edray28 <edraytuyor@gmail.com>
2022-06-21 10:18:17 +0800

    Removed Parameters, Added QR scanner

--- src/App.js
+++ src/App.js
@@ -4,26 +4,41 @@ import { Box, Typography, CssBaseline, Button, Input } from '@mui/material';
 import { Container } from '@mui/system';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import QrCodeIcon from '@mui/icons-material/QrCode';
-
+import QrCode2Icon from '@mui/icons-material/QrCode2';
 
 export default function App() {
   const generateQR = () => {
     const QRimg = {
-      "backgroundColor": "38-38-38",
-      "qrColor": "255-255-255",
-      "padding": 1,
       "data": "dev.to"
     }
     const input = document.querySelector("Input")
     if (input.value) {
       const img = document.querySelector("img");
       QRimg.data = input.value || "dev.to";
-      let parameters = `size=400x400&data=${QRimg.data}&bgcolor=${QRimg.backgroundColor}&color=${QRimg.qrColor}&qzone=${QRimg.padding}`;
+      let parameters = `size=400x400&data=${QRimg.data}`;
       img.src = `https://api.qrserver.com/v1/create-qr-code/?${parameters}`
     } else {
       alert("Please Enter a Text")
     }
+  }
 
+  const uploadQR = (e) => {
+    const form = document.querySelector("form");
+    const QRtext = form.querySelector("p")
+
+    function fetchRequest(formData) {
+      QRtext.innerText = "Scanning QR...";
+      fetch("http://api.qrserver.com/v1/read-qr-code/",{
+        method: "POST", body: formData
+      }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        QRtext.innerText = result;
+      }) 
+    }
+    let file = e.target.files[0];
+    let formData = new FormData();
+    formData.append("file",file);
+    fetchRequest(formData);
   }
   const darkTheme = createTheme({
     palette: {
@@ -40,7 +55,9 @@ export default function App() {
           marginTop: 8
         }} >
           <CssBaseline />
-          <Typography variant='h3'  >QR Code Generator </ Typography >
+          <QrCode2Icon/> 
+          <Typography variant='h3'>QR Code Generator </ Typography >
+          <QrCode2Icon/> 
         </Box>
         <Box sx={{
           display: 'flex',
@@ -53,10 +70,20 @@ export default function App() {
         <Box sx={{
           display: 'flex',
           alignItems: 'center',
-          justifyContent: "space-around",
+          justifyContent: "space-between",
           marginTop: 2
         }}>
           <Button variant="contained" color="primary" size='large' startIcon={<QrCodeIcon />} onClick={generateQR}>Create QR</Button>
+          <Box sx={{
+            width: 300
+          }}>
+          <form action='#'>
+            <label htmlFor="contained-button-file">
+              <Input accept="image/*" id="uploadQR" type="file" onChange={uploadQR}  />
+            </label>
+            <Typography component="p">Upload QR Code to Scan</Typography>
+          </form>
+          </Box>
         </Box>
         <Box sx={{
           display: 'flex',
