fdb804ff59c7e0a305925b1f9b901a1987de6cb3
edray28 <edraytuyor@gmail.com>
2022-07-12 10:47:35 +0800

    Version: 0.2.0: Added Comments, Removed setupTests, App.test, reportWebVitals & Fixed Text positions QR Scan w/TimeOut, and Added Error Indicators, Changed fetch API http to https

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import QrCodeIcon from '@mui/icons-material/QrCode';
 import QrCode2Icon from '@mui/icons-material/QrCode2';
 
 export default function App() {
+
+  /*QR Generator */
   const generateQR = () => {
     const QRimg = {
       "data": "dev.to"
@@ -22,25 +24,41 @@ export default function App() {
     }
   }
 
+  /*QR Upload and Scan */
   const uploadQR = (e) => {
     e.preventDefault();
     const form = document.querySelector("form");
+    const title = document.querySelector("h3")
+    // The Reader Text
     const QRtext = form.querySelector("p")
     
     function fetchRequest(formData) {
-      QRtext.innerText = "Scanning QR...";
-      fetch("http://api.qrserver.com/v1/read-qr-code/",{
+      title.innerText = "Scanning QR...";
+      fetch("https://api.qrserver.com/v1/read-qr-code/",{
         method: "POST", body: formData
       }).then(res => res.json()).then(result => {
         result = result[0].symbol[0].data;
-        QRtext.innerText = result;
-      }) 
+        QRtext.textContent = result;
+
+        //Condition to Check whether QR Scan has no text or invalid QR scan
+        if (QRtext.textContent === "") {
+          QRtext.textContent = "Invalid QR Code, Please Try a Valid QR"
+        }
+
+        setTimeout(() => {
+          title.innerText = "QR Code Generator";
+        }, 1000);
+      }).catch(err => {
+        title.textContent = `API ERROR! Status: ${err}`;
+      })
     }
     let file = e.target.files[0];
     let formData = new FormData();
     formData.append("file",file);
     fetchRequest(formData);
   }
+
+
   const darkTheme = createTheme({
     palette: {
       mode: 'dark',
@@ -78,14 +96,17 @@ export default function App() {
           <Box sx={{
             width: 300
           }}>
+            {/*QR Image Upload and Text Decode */}
           <form action='#'>
             <label htmlFor="contained-button-file">
               <Input accept="image/*" id="uploadQR" type="file" onChange={uploadQR}  />
             </label>
-            <Typography component="p">Upload QR Code to Scan</Typography>
+            <Typography component="p" align='justify'>Upload QR Code to Scan </Typography>
           </form>
           </Box>
         </Box>
+
+        {/*QR Image Holder */}
         <Box sx={{
           display: 'flex',
           alignItems: 'center',
@@ -95,6 +116,7 @@ export default function App() {
           <img alt='' src={""} />
         </Box>
 
+        {/*QR Github Link Page */}
         <Box sx={{
           display: 'flex',
           alignItems: 'center',
